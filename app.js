0eb39aec7f859d603bce4e68fa3c22d211847426
duddn123 <88173119+duddn123@users.noreply.github.com>
2022-01-26 15:37:48 +0900

    greeting, localStorage

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const anchor = document.querySelector("a");
+HIDDEN_CLASSNAME = "hidden";
 
 function handleSubmit(event){
     event.preventDefault();
-    console.dir(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", handleSubmit);
-
-
-function handleClicked(event){
-    event.preventDefault();
-    console.dir(event);
-    alert("go to google!");
-}
-
-anchor.addEventListener("click", handleClicked);
