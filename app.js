96713821baffe51735c2df5add2f3e5f028842c6
duddn123 <88173119+duddn123@users.noreply.github.com>
2022-01-26 12:01:00 +0900

    preventDefault practice

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const anchor = document.querySelector("a");
+
 function handleSubmit(event){
     event.preventDefault();
     console.dir(loginInput.value);
 }
 
 loginForm.addEventListener("submit", handleSubmit);
+
+
+function handleClicked(event){
+    event.preventDefault();
+    console.dir(event);
+    alert("go to google!");
+}
+
+anchor.addEventListener("click", handleClicked);
