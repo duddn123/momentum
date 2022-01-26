71a635b6578ae297939fdd0c57e22b5f643cc8d0
duddn123 <88173119+duddn123@users.noreply.github.com>
2022-01-26 18:10:19 +0900

    updated clock

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,13 @@
 const clock = document.querySelector("#clock");
 
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, 0);
+    const minutes = String(date.getMinutes()).padStart(2, 0);
+    const seconds = String(date.getSeconds()).padStart(2, 0);
+    clock.innerText = `${hour}:${minutes}:${seconds}`;
 }
 
-setInterval(sayHello, 2000);
+getClock()
+setInterval(getClock, 1000);
