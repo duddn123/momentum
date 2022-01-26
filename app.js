cc2f850d68907d77bc01d0a7d7958724c8428cfa
duddn123 <88173119+duddn123@users.noreply.github.com>
2022-01-26 16:13:17 +0900

    maintaining login satus

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 HIDDEN_CLASSNAME = "hidden";
+USERNAME_KEY = "username";
 
 function handleSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
+}
+
+function paintGreeting(username){
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", handleSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", handleSubmit);
+}
+else{
+    paintGreeting(savedUsername);
+}
