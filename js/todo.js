d564250d5132438cfc0356cb3aaccd5cf7d31533
duddn123 <88173119+duddn123@users.noreply.github.com>
2022-02-08 11:21:29 +0900

    delete to do

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,22 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+
+function handleDelete(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintList(newToDo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
+    button.addEventListener("click", handleDelete);
 }
 
 function handleToDoForm(event) {
