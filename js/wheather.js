12610be4154f2fa1ec47aa92eb8072d3628a5ef9
duddn123 <88173119+duddn123@users.noreply.github.com>
2022-02-09 13:00:04 +0900

    weather

---
+++ js/wheather.js
@@ -0,0 +1,19 @@
+const API_KEY = "a166be9e457bd5c1006a98212fb5973e";
+
+function success(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json().then(data => {
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }));
+}
+
+function error(){
+    console.log("I can't find your location.")
+}
+
+navigator.geolocation.getCurrentPosition(success, error);
